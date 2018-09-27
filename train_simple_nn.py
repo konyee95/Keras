5e491f993dc20eddacff5d25d7970057df1cb29a
Chin Kon Yee (Top Glove - F09) <chinky@topglove.tg.local>
2018-09-27 12:03:52 +0800

    Compiled Keras model

--- train_simple_nn.py
+++ train_simple_nn.py
@@ -76,3 +76,15 @@ model = Sequential()
 model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
 model.add(Dense(512, activation="sigmoid"))
 model.ass(Dense(lb.classes_), activation="softmax")
+
+# initialize our initial learning rate and # of epochsto train for
+INIT_LR = 0.01
+EPOCHS = 75
+
+# compile the model using SGD as our optimizer and categorical
+# cross-entropy loss (you'll wany to use binary_crossentrophy
+# for 2-class classification)
+print("[INFO] training network...")
+opt = SGD(lr=INIT_LR)
+model.compile(loss="categorical_crosstropy", optimizer=opt,
+    metrics=["accuracy"])
